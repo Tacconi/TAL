91008b7b07a8e29303b4b978dd46588c9722c39f
Lorenzo Tacconi <lorenzo.tacconi6@gmail.com>
2016-01-05 02:30:50 +0100

    Aggiunte tutte le classi parziali

---
+++ TimeAndLapseTEST/TimeAndLapseLibrary/Data/Classi/Point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeAndLapseLibrary.Data.Designer
+{
+    public partial class Point
+    {
+        partial void InitializePartial()
+        {
+            PointId = new Guid();
+        }
+    }
+}

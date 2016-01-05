6057c4bbafc89e597384f88c6683cc8674b7006f
Lorenzo Tacconi <lorenzo.tacconi6@gmail.com>
2016-01-05 02:09:19 +0100

    Aggiunte Classi

---
+++ TimeAndLapseTEST/TimeAndLapseLibrary/Data/Classi/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeAndLapseLibrary.Data.Classi
+{
+    public partial class Player
+    {
+        partial void InitializePartial()
+        {
+            PlayerId = new Guid();
+        }
+    }
+}

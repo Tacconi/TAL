1d79630279a32a33c3612d0edc185ef63a9e9566
Lorenzo Tacconi <lorenzo.tacconi6@gmail.com>
2016-01-05 01:51:30 +0100

    Aggiunto Entity Framework E POCO Generator

---
+++ TimeAndLapseTEST/TimeAndLapseLibrary/TimeAndLapseContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeAndLapseLibrary
+{
+    public class TimeAndLapseContext : DbContext
+    {
+        public TimeAndLapseContext() : base("name=TimeAndLapseContext")
+        {
+            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<School, Migrations.Configuration>());
+            Database.SetInitializer<TimeAndLapseContext>(null);
+        }
+        public DbSet<Timing> Timings { get; set; }
+        public DbSet<TimingType> TimingTypes { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Player> Players { get; set; }
+    }
+}
+}
